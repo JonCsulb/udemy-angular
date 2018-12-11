c8d4485884dcb89d3647de0272519c50253310ad
Jon <jonngu95@gmail.com>
2018-12-11 10:55:01 -0800

    Save before working on Udemy tutorial Dec 11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactautoComponent } from './contactauto/contactauto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    ContactautoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contactauto/contactauto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactauto',
+  templateUrl: './contactauto.component.html',
+  styleUrls: ['./contactauto.component.css']
+})
+export class ContactautoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
